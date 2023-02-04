14274f40d657ef2aa20aa32b7a3cababf4792a86
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-02-04 06:27:07 -0500

    add mocks contracts, script and simple tests for them

---
+++ scripts/mocks/deploy-MockERC1155Upgradeable.ts
@@ -0,0 +1,19 @@
+import { ethers, addressBook, network } from 'hardhat'
+
+async function main() {
+    const [deployer] = await ethers.getSigners()
+
+    const MockERC1155Upgradeable = await ethers.getContractFactory('MockERC1155')
+    const mockERC1155Upgradeable = await MockERC1155Upgradeable.deploy()
+
+    await mockERC1155Upgradeable.deployed()
+    await addressBook.saveContract('MockERC1155Upgradeable', mockERC1155Upgradeable.address, network.name, deployer.address)
+    await mockERC1155Upgradeable.initialize('MockERC1155', 'MOCK', 'https://google.com')
+
+    console.log('MockERC1155Upgradeable deployed to:', mockERC1155Upgradeable.address)
+}
+
+main().catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+})
---
+++ scripts/mocks/deploy-MockERC20Upgradeable.ts
@@ -0,0 +1,24 @@
+import { ethers, addressBook, network } from 'hardhat'
+
+async function main() {
+    const [deployer] = await ethers.getSigners()
+
+    const MockERC20Upgradeable = await ethers.getContractFactory('MockERC20Upgradeable')
+    const mockERC20Upgradeable = await MockERC20Upgradeable.deploy()
+
+    await mockERC20Upgradeable.deployed()
+    await addressBook.saveContract(
+        'MockERC20Upgradeable',
+        mockERC20Upgradeable.address,
+        network.name,
+        deployer.address
+    )
+    await mockERC20Upgradeable.initialize('MockERC20Upgradeable', 'MOCK')
+
+    console.log('MockERC20Upgradeable deployed to:', mockERC20Upgradeable.address)
+}
+
+main().catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+})
