ad39892a7a0288580fce9cf2b24676a31b715b2f
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-02-22 06:01:11 -0500

    start working on a extended version

---
+++ contracts/MyMultiSigExtended.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import './MyMultiSig.sol';
+
+contract MyMultiSigExtended is MyMultiSig {
+  bool private _onlyOwnerRequest;
+
+  constructor(
+    string memory name_,
+    address[] memory owners_,
+    uint16 threshold_,
+    bool isOnlyOwnerRequest_
+  ) MyMultiSig(name_, owners_, threshold_) {
+    _onlyOwnerRequest = isOnlyOwnerRequest_;
+  }
+
+  /// @notice Retrieves if the contract only accepts owner requests (use for UI and other integrations)
+  /// @return The true if the contract only accepts owner requests, false otherwise.
+  function allowOnlyOwnerRequest() public view returns (bool) {
+    return _onlyOwnerRequest;
+  }
+
+  /// @notice Set if the contract only accepts owner requests (use for UI and other integrations)
+  /// @param isOnlyOwnerRequest_ The true if the contract only accepts owner requests, false otherwise.
+  /// @dev This function can only be called inside a multisig transaction.
+  function setOnlyOwnerRequest(bool isOnlyOwnerRequest_) public onlyThis {
+    _onlyOwnerRequest = isOnlyOwnerRequest_;
+  }
+}
