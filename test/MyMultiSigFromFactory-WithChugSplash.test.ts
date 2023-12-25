09836fdbe4979e95ffcf97d1dd22206ee456b769
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-12-25 15:17:03 -0500

    remove chugsplash (#132)

--- test/MyMultiSigFromFactory-WithChugSplash.test.ts
+++
@@ -1,6 +0,0 @@
-import { MyMultiSigStandardTests, MyMultiSigExtendedTests, DeploymentType } from './shared/tests'
-
-describe('MyMultiSig - Deployed From Factory With ChugSplash', function () {
-  MyMultiSigStandardTests(DeploymentType.WithChugSplash)
-  MyMultiSigExtendedTests(DeploymentType.WithChugSplash)
-})
